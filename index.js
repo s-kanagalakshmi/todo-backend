d0ea428b83bb4b50faeecffffc234a5364de08e1
s-kanagalakshmi <kanagalakshmi.mca16@gmail.com>
2025-04-04 14:10:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(cors())
+app.use(express.json())
+const mongoose = require('mongoose');
+//Database connection
+mongoose.connect("mongodb+srv://kanagalakshmimca16:EXyXoHq2joi1VGBF@todolist-cluster.9kwsquz.mongodb.net/todolist?retryWrites=true&w=majority&appName=todolist-cluster")
+.then(() => { console.log("Database connected") })
+    .catch(() => {
+        console.log("Database not connected")
+    })
+// const inputArray=["Apple","Banana"]
+//Model creation
+const fruitlist = mongoose.model("list", { name: String }, "fruit")
+
+app.listen(3000, () => {
+    console.log("Server started")
+})
+app.get("/", (req, res) => {
+    res.send("DB started")
+})
+
+app.post("/addfruit", (req, res) => {
+    let newfruit = req.body.newfruit
+
+    let addedfruit = new fruitlist({
+        name: newfruit
+    })
+    addedfruit.save().then(() => { console.log("List updated") })
+})
+app.get("/fruits", (req, res) => {
+    fruitlist.find().
+        then((data1) => {
+
+            res.send(data1)
+        })
+
+        .catch((error) => { console.log(error) })
+})
+app.delete("/deletefruit", (req, res) => {
+    let { newfruit } = req.body;
+    fruitlist.deleteOne({ name: newfruit })
+        .then(() => {
+            console.log(newfruit, "deleted");
+            res.send({ message: "Deleted successfully" });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ error: "Failed to delete" });
+        });
+});
